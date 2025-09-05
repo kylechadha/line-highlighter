71649eb6bd7d0e47bc92fff86bb6f3917cbfa3f3
Kyle Chadha <kyle.chadha@gmail.com>
2025-09-05 10:56:46 -0700

    Add development tooling and tests

---
+++ playwright.config.js
@@ -0,0 +1,29 @@
+const { defineConfig, devices } = require('@playwright/test');
+
+module.exports = defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: 'html',
+  use: {
+    trace: 'on-first-retry',
+  },
+
+  projects: [
+    {
+      name: 'chromium',
+      use: { 
+        ...devices['Desktop Chrome'],
+        // Load extension from local directory
+        launchOptions: {
+          args: [
+            `--disable-extensions-except=${__dirname}`,
+            `--load-extension=${__dirname}`
+          ],
+        }
+      },
+    },
+  ],
+});
